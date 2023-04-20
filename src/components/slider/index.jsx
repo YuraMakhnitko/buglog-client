3ac93a72e253793b6d12334e578f006f16d91bfa
YuraMakhnitko <bizoru82@gmail.com>
2023-04-20 13:10:56 +0300

    some changes in slider

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -12,7 +12,8 @@ const HomeSlider = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
+    arrows: false,
   };
   return (
     <Slider {...settings}>
